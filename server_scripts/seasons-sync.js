5022d259f272e9b514d2291920783c54e36fb610
Titanor Soulstone <sir.titas@yahoo.com.au>
2025-01-14 19:51:15 +0800

    Added in-game settings

--- server_scripts/seasons-sync.js
+++ server_scripts/seasons-sync.js
@@ -1,7 +1,8 @@
 // Settings
 
 // change this to "south" for the southern hemisphere
-var locale = "north"
+// var locale = "north"
+var cmdloc
 
 // Code
 
@@ -37,21 +38,61 @@ var seasonsSouth = [ // for servers in the southern hemisphere
 	"early_summer"
 ]
 
-function seasonChange() {
-	if ( locale == "north" ) {
+function seasonChange(loc) {
+	if ( loc == "north" ) {
 		Utils.server.runCommand('season set '+seasonsNorth[month])
-	} else if ( locale == "south" ) {
+	} else if ( loc == "south" ) {
 		Utils.server.runCommand('season set '+seasonsSouth[month])
 	}
 }
 
+ServerEvents.commandRegistry(event => {
+	const { commands: Commands, arguments: Arguments } = event
+
+	event.register
+		(Commands.literal('locale') // The name of the command
+			.requires(s => s.hasPermission(2)) // Check if the player has operator privileges
+			.executes(ctx => {
+				//the variable holding the current locale is cmdloc
+				let player = ctx.source.player;
+				//player.displayClientMessage(Component.white('the current hemisphere is'+cmdloc+"ern"));
+				Utils.server.runCommand('say you are in the '+cmdloc+"ern hemisphere")
+				return 1
+			})
+			.then(
+				Commands.argument(
+					'hemisphere', 
+					Arguments.STRING.create(event)
+				) // the selected hemisphere  --  must be north or south
+				.executes(ctx => {
+					let player = ctx.source.player;
+					var newloc = Arguments.STRING.getResult(ctx, 'hemisphere');
+					if (newloc == "north" || newloc == "south") {
+						cmdloc = newloc
+						Utils.server.runCommand("say set server to "+newloc+"ern hemisphere")
+					} else {
+						//player.displayClientMessage(Component.white('hemisphere must be \'north\' or \'south\''));
+						Utils.server.runCommand('say hemisphere must be \'north\' or \'south\'');
+						Utils.server.runCommand('say youre input was: ' + newloc);
+					}
+					return 1
+				})
+			)
+		)
+	}
+)
+
 ServerEvents.loaded(event => {
+	if (!event.server.persistentData.locale) {
+		event.server.persistentData.locale = "north"
+	}
+	cmdloc = event.server.persistentData.locale
 	Utils.server.runCommand('gamerule doSeasonCycle false')
-	seasonChange()
-	if ( locale == "north" ) {
+	seasonChange(event.server.persistentData.locale)
+	if ( event.server.persistentData.locale == "north" ) {
 		console.info(`server started the current season is ` + seasonsNorth[month])
 		//Utils.server.runCommand(`say server started the current season is ` + seasonsNorth[month])
-	} else if ( locale == "south" ) {
+	} else if ( event.server.persistentData.locale == "south" ) {
 		console.info(`server started the current season is ` + seasonsSouth[month])
 		//Utils.server.runCommand(`say server started the current season is ` + seasonsSouth[month])
 	} else {
@@ -61,12 +102,16 @@ ServerEvents.loaded(event => {
 })
 
 ServerEvents.tick(event => {
+	if (event.server.persistentData.locale != cmdloc) {
+		event.server.persistentData.locale = cmdloc
+		seasonChange(event.server.persistentData.locale)
+	}
 	newMonth = new Date().getMonth()
 	if (newMonth != month) {
 		month = newMonth
 		if (month == 2 || month == 5 || month == 8 || month == 11) { // javascript months go from 0 to 11 instead of 1 to 12
 			Utils.server.runCommand(`say the seasons are changing`)
 		}
-		seasonChange()
+		seasonChange(event.server.persistentData.locale)
 	}
 })
